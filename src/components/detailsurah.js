ccc86cdb705508dfe24504cd4e1681d82821ef10
ansxy <53747702+ansxy@users.noreply.github.com>
2022-11-30 19:18:34 +0800

    fix play button and pause button is now working

--- src/components/detailsurah.js
+++ src/components/detailsurah.js
@@ -1,15 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import { useLoaderData, useLocation } from "react-router-dom";
-import { BsFillPlayFill, BsStopFill } from "react-icons/bs";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
 import { AiOutlineBook } from "react-icons/ai";
 import { IconContext } from "react-icons";
 export default function DetailSurah() {
   const state = useLoaderData();
-
-  // useEffect(() => {
-  //     setPlay(state);
-  // }, [state]);
-
   const handlePlay = (index) => {
     state.map((arr, i) => {
       if (i === index) {
@@ -21,31 +16,64 @@ export default function DetailSurah() {
     });
   };
 
-  // const handlePause = (index) => {
-  //   setAudio((arr) =>
-  //     arr.map((sound, i) => {
-  //       if (i === index) {
-  //         sound.audio.pause();
-  //         return { ...sound, play: false };
-  //       }
-  //       return { ...sound, play: false };
-  //     })
-  //   );
-  // };
+  const handlePause = (index) => {
+    state.map((arr, i) => {
+      if (i === index) {
+        arr.alafasy.pause();
+        return { ...arr, play: false };
+      }
+      return { ...arr, play: false };
+    });
+  };
 
-  // const tafsirModal = (e) => {
-  //   e.preventDefault();
-  // };
+  const handleFull = () => {};
+
   return (
-    <div className="flex justify-center bg-gradient-to-r from-[#141E30] to-[#243B55]">
-      <section className="flex flex-col w-4/5">
+    <div className="flex flex-col justify-center bg-gradient-to-r from-[#141E30] to-[#243B55]">
+      <section className="flex justify-center  w-full">
+        <button
+          name="play"
+          className="flex justify-start mb-5"
+          onClick={() => handleFull()}
+        >
+          <IconContext.Provider
+            value={{
+              color: "white",
+              size: "50px",
+              className: "global-class-name",
+            }}
+          >
+            <>
+              <BsFillPlayFill />
+            </>
+          </IconContext.Provider>
+        </button>
+        <button
+          name="stop"
+          className="flex justify-start  mb-5"
+          onClick={() => handlePause()}
+        >
+          <IconContext.Provider
+            value={{
+              color: "white",
+              size: "50px",
+              className: "global-class-name",
+            }}
+          >
+            <>
+              <BsFillPauseFill />
+            </>
+          </IconContext.Provider>
+        </button>
+      </section>
+      <section className="flex flex-col w-full justify-center place-items-center">
         {!state ? (
           <></>
         ) : (
           <>
             {state.map((data, i) => {
               return (
-                <div className="p-10 flex flex-col" key={i}>
+                <div className="p-10 flex flex-col w-4/5" key={i}>
                   <div
                     tabIndex={0}
                     className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box w-28 mb-5"
@@ -72,65 +100,67 @@ export default function DetailSurah() {
                         htmlFor={data.number.inSurah}
                         className="modal cursor-pointer"
                       >
-                        <div className="modal-box relative scrollbar-hide" htmlFor="">
-                          <h3 className=" py-4 text-lg font-bold">Tafsir</h3>
+                        <div
+                          className="modal-box relative scrollbar-hide"
+                          htmlFor=""
+                        >
+                          <h3 className=" py-4 text-3xl font-bold">Tafsir</h3>
                           <div>
-                            <h3 className="text-md font-bold"> Jalalayn </h3>
+                            <h3 className="text-xl font-bold m-3">
+                              {" "}
+                              Jalalayn{" "}
+                            </h3>
                             <p className="">{data.tafsir["jalalayn"]}</p>
                           </div>
                           <div>
-                            <h3 className="text-md font-bold"> Kemenag </h3>
+                            <h3 className="text-xl font-bold m-3"> Kemenag </h3>
                             <p className="">{data.tafsir["kemenag"].short}</p>
                           </div>
                           <div>
-                            <h3 className="text-md font-bold"> Quraish </h3>
+                            <h3 className="text-xl font-bold m-3"> Quraish </h3>
                             <p className="">{data.tafsir["quraish"]}</p>
                           </div>
                         </div>
                       </label>
                     </div>
                   </div>
-                  {data.play ? (
-                    <>
-                      <button
-                        name="play"
-                        className="flex justify-start ml-5 mb-5"
-                        onClick={() => handlePlay(i)}
+                  <div className="flex felx-col max-w-content">
+                    <button
+                      name="play"
+                      className="flex justify-start mb-5"
+                      onClick={() => handlePlay(i)}
+                    >
+                      <IconContext.Provider
+                        value={{
+                          color: "white",
+                          size: "50px",
+                          className: "global-class-name",
+                        }}
                       >
-                        <IconContext.Provider
-                          value={{
-                            color: "white",
-                            size: "50px",
-                            className: "global-class-name",
-                          }}
-                        >
-                          <>
-                            <BsFillPlayFill />
-                          </>
-                        </IconContext.Provider>
-                      </button>
-                    </>
-                  ) : (
-                    <>
-                      <button
-                        name="stop"
-                        className="flex justify-start ml-5 mb-5"
-                        onClick={() => handlePlay(i)}
+                        <>
+                          <BsFillPlayFill />
+                        </>
+                      </IconContext.Provider>
+                    </button>
+
+                    <button
+                      name="stop"
+                      className="flex justify-start  mb-5"
+                      onClick={() => handlePause(i)}
+                    >
+                      <IconContext.Provider
+                        value={{
+                          color: "white",
+                          size: "50px",
+                          className: "global-class-name",
+                        }}
                       >
-                        <IconContext.Provider
-                          value={{
-                            color: "white",
-                            size: "50px",
-                            className: "global-class-name",
-                          }}
-                        >
-                          <>
-                            <BsStopFill />
-                          </>
-                        </IconContext.Provider>
-                      </button>
-                    </>
-                  )}
+                        <>
+                          <BsFillPauseFill />
+                        </>
+                      </IconContext.Provider>
+                    </button>
+                  </div>
 
                   <p className="text-right text-2xl font-bold">
                     <span className="border-4 rounded-full p-1 mr-2">
